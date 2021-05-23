e4354f7c2094b0296a4146d56594c06c906eb562
Naveen <naveentruly86@gmail.com>
2021-05-23 21:17:17 +0530

    Added: Welcome Message

---
+++ src/main/java/com/addressproblem/AddressBookMain.java
@@ -0,0 +1,7 @@
+package com.addressproblem;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Address Book Problem in AddressBookMain Class");
+    }
+}
