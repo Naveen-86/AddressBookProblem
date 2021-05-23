cfdcfab2910e0e7052f55e17890089df49eab33c
Naveen <naveentruly86@gmail.com>
2021-05-23 21:23:10 +0530

    Added: Create Contacts of Address Book

--- src/main/java/com/addressproblem/AddressBookMain.java
+++ src/main/java/com/addressproblem/AddressBookMain.java
@@ -1,7 +1,26 @@
 package com.addressproblem;
 
 public class AddressBookMain {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Address Book Problem in AddressBookMain Class");
+            public static void main(String[] args){
+            System.out.println("Welcome to Address Book Program");
+        }
     }
-}
+
+    class Contacts{
+
+        String firstName, lastName, address, cityName, state, email;
+        int zip;
+        long phoneNum;
+
+        Contacts(String fN, String lN, String addr, String city, String st, int z, long pNum, String eId){
+            firstName = fN;
+            lastName = lN;
+            address = addr;
+            cityName = city;
+            state = st;
+            zip = z;
+            phoneNum = pNum;
+            email = eId;
+
+        }
+    }
