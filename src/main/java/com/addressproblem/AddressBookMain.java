393fbcc49fc1ca50d43f9935304a270c396d30c7
Naveen Madhaboina <79349929+Naveen-86@users.noreply.github.com>
2021-05-25 16:01:26 +0530

    Update AddressBookMain.java

--- src/main/java/com/addressproblem/AddressBookMain.java
+++ src/main/java/com/addressproblem/AddressBookMain.java
@@ -1,26 +1,46 @@
 package com.addressproblem;
+import java.util.Scanner;
 
 public class AddressBookMain {
-            public static void main(String[] args){
-            System.out.println("Welcome to Address Book Program");
-        }
-    }
+	
+	public static void main(String[] args) {
+		Details c = new Details();
+		System.out.println("Enter First Name:");
+		c.readInput("First Name :");
+		System.out.println("Enter Last Name:");
+		c.readInput("First Name :");
+		System.out.println("Enter Address :");
+		c.readInput("First Name :");
+		System.out.println("Enter City Name:");
+		c.readInput("First Name :");
+		System.out.println("Enter State Name:");
+		c.readInput("First Name :");
+		System.out.println("Enter Zip:");
+		c.readInput1();
+		System.out.println("Mobile Number:");
+		c.readInput1();
+		System.out.println("Enter Email:");
+		c.readInput("First Name :");
+		System.out.println("Address Book is Created, THANK YOU");
+		
+	}
+}
 
-    class Contacts{
+class Details{
+	
+	String readInput(String stg)
+	{
+		Scanner stn = new Scanner(System.in);
+		String str = stn.nextLine();
+		return str;
+	}
+	
+	long readInput1()
+	{
+		Scanner n = new Scanner(System.in);
+		long s = n.nextLong();
+		return s;	
+	}
+	
+}
 
-        String firstName, lastName, address, cityName, state, email;
-        int zip;
-        long phoneNum;
-
-        Contacts(String fN, String lN, String addr, String city, String st, int z, long pNum, String eId){
-            firstName = fN;
-            lastName = lN;
-            address = addr;
-            cityName = city;
-            state = st;
-            zip = z;
-            phoneNum = pNum;
-            email = eId;
-
-        }
-    }
