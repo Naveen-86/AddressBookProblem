ac71d1dd63a6d19a17f78fc4642b48119f3863d7
Naveen <naveentruly86@gmail.com>
2021-06-06 11:35:40 +0530

    Added: Ability to get Person by Count

---
+++ src/main/java/com/addressproblem/ScannerForUserInput.java
@@ -0,0 +1,14 @@
+package com.addressproblem;
+
+import java.util.Scanner;
+
+public class ScannerForUserInput {
+
+    /**
+     * Scanner class usage
+     */
+    public Scanner scannerInitializer(){
+        Scanner scanner = new Scanner(System.in);
+        return scanner;
+    }
+}
