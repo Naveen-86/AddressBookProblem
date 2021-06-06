ed531aaf8e311a5f0d22e60b723624814c1ddebd
Naveen <naveentruly86@gmail.com>
2021-06-06 11:20:03 +0530

    Added: Ability to Search Person By City or Name

--- src/main/java/com/addressproblem/AddressBookSimulator.java
+++ src/main/java/com/addressproblem/AddressBookSimulator.java
@@ -2,11 +2,12 @@ package com.addressproblem;
 
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 /**
  * main class to maintain multiple contact books
  */
-public class AddressBookSimulator {
+public class AddressBookSimulator{
     static HashMap<String, ContactManager> booksMap = new HashMap<>();
 
     /**
@@ -15,7 +16,7 @@ public class AddressBookSimulator {
     public static void main(String[] args) {
         boolean isExit = false;
         while(!isExit) {
-            System.out.println("Select options: \n1.Add Book\n2.AccessBook\n3.Exit");
+            System.out.println("Select options: \n1.Add Book\n2.AccessBook\n3.search contact\n4.Exit");
             Scanner scanner = new Scanner(System.in);
             int option = scanner.nextInt();
 
@@ -27,7 +28,7 @@ public class AddressBookSimulator {
                     booksMap.put(bookName, new ContactManager());
                     break;
                 case 2:
-                    System.out.println("Enter the name of new book to access it");
+                    System.out.println("Enter the name of the book to access it");
                     Scanner scanner2 = new Scanner(System.in);
                     Object bookName1 = scanner2.nextLine();
                     if(booksMap.containsKey(bookName1)) {
@@ -35,6 +36,12 @@ public class AddressBookSimulator {
                         contactManager.accessContact();
                         contactManager.getContactList();
                     }
+                    break;
+                case 3:
+                    System.out.println("Enter the name to search");
+                    Scanner scanner3 = new Scanner(System.in);
+                    String name = scanner3.nextLine();
+                    System.out.println(booksMap.values().stream().flatMap(book -> book.getContactList().stream().filter(contact -> contact.getSTATE().equals(name) || contact.getCITY().equals(name))).collect(Collectors.toList()));
                     break;
                 default:
                     isExit = true;
